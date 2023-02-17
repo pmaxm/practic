efa3bf869a74915bd71e797cde512db90d322b59
user123 <user123@gmail.com>
2023-02-17 08:16:43 +0300

    Added a request for the user's last name

--- main.java
+++ main.java
@@ -3,12 +3,12 @@ package org;
 public class main {
 
     public static void main(String[] args) {
-
-		Scanner in = new Scanner(System.in);
-        System.out.println("Hello, World!");
-		System.out.println("Enter your name: ");
+	    Scanner in = new Scanner(System.in);
+        System.out.println("Enter your lastname: ");
         String name = in.nextLine();
-        System.out.println("Hello, " + lastname);
+        System.out.println("Enter your name: ");
+        String lastname = in.nextLine();
+        System.out.println("Hello, " + lastname +" " + name);
     }
 
 }
