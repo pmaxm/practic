51b471077569b6fc9ac201f35defd06bf65a76ba
user123 <user123@gmail.com>
2023-02-17 08:12:48 +0300

    Now Hello a personalized

--- main.java
+++ main.java
@@ -4,8 +4,11 @@ public class main {
 
     public static void main(String[] args) {
 
+		Scanner in = new Scanner(System.in);
         System.out.println("Hello, World!");
-  
+		System.out.println("Enter your name: ");
+        String name = in.nextLine();
+        System.out.println("Hello, " + lastname);
     }
 
 }
