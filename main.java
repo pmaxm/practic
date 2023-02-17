f2fb7e0451058a7c85522fc4818dc99c3a377719
user123 <user123@gmail.com>
2023-02-17 08:18:46 +0300

    Added comma.

--- main.java
+++ main.java
@@ -4,11 +4,12 @@ public class main {
 
     public static void main(String[] args) {
 
-		Scanner in = new Scanner(System.in);
-        System.out.println("Hello, World!");
-		System.out.println("Enter your name: ");
+	    Scanner in = new Scanner(System.in);
+        System.out.println("Enter your lastname: ");
         String name = in.nextLine();
-        System.out.println("Hello, " + lastname);
+        System.out.println("Enter your name: ");
+        String lastname = in.nextLine();
+        System.out.println("Hell0, " + lastname +" " + name);
     }
 
 }
